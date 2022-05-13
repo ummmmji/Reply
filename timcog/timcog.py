320a68c495317aa9d9d1212b42fb3afbbeed5502
ummmmji <84630719+ummmmji@users.noreply.github.com>
2022-05-13 22:30:28 +0800

    Update timcog.py

--- timcog/timcog.py
+++ timcog/timcog.py
@@ -12,3 +12,6 @@ async def on_message(self, message):
 
         if message.content.startswith('笑死'):
             await message.reply('笑死', mention_author=False)
+            
+        if message.content.startswith('婆'):
+            await message.reply('小浠我婆', mention_author=False)
